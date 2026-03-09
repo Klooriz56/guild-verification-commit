0e3f92d254c791158094c953d560b6f9309733af
Klooriz56 <klooris56@outlook.com>
2026-03-09 12:12:17 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:12:02.668Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-09T09:12:16.893Z
+Commit: Fix broken links in documentation
