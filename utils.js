8b41af66a490efa3906dc948153ce3cf8d6d8ae0
Klooriz56 <klooris56@outlook.com>
2026-03-09 12:12:40 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:12:16.893Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-09T09:12:39.940Z
+Commit: Fix memory leak in event handlers
