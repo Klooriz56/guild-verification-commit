dc467bdd422953918da87a2b685ec70d32028d18
Klooriz56 <klooris56@outlook.com>
2026-03-09 12:13:54 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:13:31.428Z
-Commit: Add multi-language support
+Timestamp: 2026-03-09T09:13:53.857Z
+Commit: Resolve null pointer exception in user service
