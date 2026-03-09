13c89dbba539cf04bf4c734c03302a2bc1aaf3f4
Klooriz56 <klooris56@outlook.com>
2026-03-09 12:14:37 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:14:27.479Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T09:14:37.263Z
+Commit: Resolve timezone issues in date handling
